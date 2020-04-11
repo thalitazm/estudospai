e6096a3a15237c6250b30470f65a0e2ebfc89237
Thalita Zanetti <58237778+thalitazm@users.noreply.github.com>
2020-04-11 18:09:02 -0300

    upload file
    o arquivo antigo estava com erro

--- rotate.py
+++ rotate.py
@@ -8,7 +8,7 @@ panda = cv2.imread('panda.jpg')
 centro = (lar // 2, alt // 2) #acha o centro
 
 M = cv2.getRotationMatrix2D(centro, 30, 1.0) #rotaciona 30 graus
-panda_rotate = cv2.warpAffine(panda, M[(lar, alt)])
+panda_rotate = cv2.warpAffine(panda, M, (lar, alt))
 
 cv2.imshow("Panda rotate", panda_rotate)
 cv2.waitKey(0)
