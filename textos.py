cd31cd3e8337db0859f1a9b9cc1e4cb7c741d553
Thalita Zanetti <58237778+thalitazm@users.noreply.github.com>
2020-04-11 00:21:50 -0300

    update textos.py
    add function imwrite

--- textos.py
+++ textos.py
@@ -7,6 +7,6 @@ charactere = cv2.QT_FONT_NORMAL
 cv2.putText(rasp, 'Python is soooooo cool!!!', (25,65), charactere, 2, (135, 165, 45), 2, cv2.LINE_AA)
 
 cv2.imshow("Raspberry", rasp)
+cv2.imwrite("testeTexto.jpg", rasp)
 cv2.waitKey(0)
-
 
