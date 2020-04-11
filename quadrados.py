28a1d7483d27cf640c7cc09ef49420f0d3bfbbd2
Thalita Zanetti <58237778+thalitazm@users.noreply.github.com>
2020-04-11 00:20:23 -0300

    update
    add function imwrite

--- quadrados.py
+++ quadrados.py
@@ -4,4 +4,5 @@ for y in range(0, imagem.shape[0], 10): #percorre linhas
     for x in range(0, imagem.shape[1], 10): #percorre colunas
         imagem[y:y+5, x: x+5] = (115,165,75) #verdepredomina
 cv2.imshow("Imagem modificada", imagem)
+cv2.imwrite("testeQuadrados.jpg", imagem)
 cv2.waitKey(0)
